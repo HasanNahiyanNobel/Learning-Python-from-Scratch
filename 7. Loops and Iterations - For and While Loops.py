b4a55e1537c51832fcf156be49fcf60ffe7b2a35
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2023-08-02 17:48:37 +0600

    Completed tutorial 7.

---
+++ 7. Loops and Iterations - For and While Loops.py
@@ -0,0 +1,15 @@
+nums = [1, 2, 3, 4, 5]
+
+for num in nums:
+    print(num)
+
+print()
+
+for num in nums:
+    for letter in 'abc':
+        print(num, letter)
+
+print()
+
+for i in range(1, 11):
+    print(i)
